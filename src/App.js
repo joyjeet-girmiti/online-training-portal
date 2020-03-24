2c3290da19af278bf5bbb85f88c720e963b6dedb
Joyjeet Paul <joyjeet.paul@shopx.in>
2020-03-24 12:58:47 +0530

    added index file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Online Training Application </h1>
+      <h1> Online Training Application Portal</h1>
     </div>
   );
 }
